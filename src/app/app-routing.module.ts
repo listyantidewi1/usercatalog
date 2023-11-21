f0fed318ed993f00027321cc1aca162ad089b8e8
listyantidewi1 <listyantidewi@gmail.com>
2023-11-22 00:35:37 +0700

    add route to include page and limit to its url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { UserdetailComponent } from './component/userdetail/userdetail.component
 
 const routes: Routes = [
   { path: 'users', component: UsersComponent },
+  { path: 'users/:page/:limit', component: UsersComponent },
   { path: 'user/:id', component: UserdetailComponent },
   { path: '**', redirectTo: 'users' },
 ];
--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Response } from '../../interface/response.interface';
 import { UserService } from './../../service/user.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -8,13 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './users.component.css',
 })
 export class UsersComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
   response: Array<any>;
+  page: number;
+  limit: number;
+
+  // ngOnInit(): void {
+  //   this.userService.getUsers(1, 5).subscribe((results: any) => {
+  //     console.log(results);
+  //     this.response = results;
+  //   });
+  // }
 
   ngOnInit(): void {
-    this.userService.getUsers(1, 5).subscribe((results: any) => {
+    this.route.params.subscribe((params) => {
+      const page = params['page'];
+      const limit = params['limit'];
+      console.log("page"+page);
+      console.log("limit"+limit);
+      this.getDataById(page, limit);
+    });
+  }
+
+  getDataById(page: number, limit: number): void {
+    this.userService.getUsers(page, limit).subscribe((results: any) => {
       console.log(results);
       this.response = results;
+      this.page = page;
+      this.limit = limit;
     });
   }
 }
