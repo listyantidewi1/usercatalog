1b9cb9d98d72bd0d439b66f94b2cd6c4ed4df760
listyantidewi1 <listyantidewi@gmail.com>
2023-11-21 23:12:28 +0700

    view all customers data in a table

--- src/app/component/userdetail/userdetail.component.ts
+++ src/app/component/userdetail/userdetail.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-userdetail',
   templateUrl: './userdetail.component.html',
-  styleUrl: './userdetail.component.css'
+  styleUrl: './userdetail.component.css',
 })
-export class UserdetailComponent {
-
-}
+export class UserdetailComponent {}
