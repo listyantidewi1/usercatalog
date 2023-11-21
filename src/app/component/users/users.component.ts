06070b297a6edcd909b2c7d7ddbacbf32a55b617
listyantidewi1 <listyantidewi@gmail.com>
2023-11-21 21:52:03 +0700

    fixed URL format

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -10,7 +10,7 @@ export class UsersComponent implements OnInit {
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.userService.getUsers(2, 10).subscribe((results: any) => {
+    this.userService.getUsers(1, 10).subscribe((results: any) => {
       console.log(results);
     });
   }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -7,16 +7,23 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
   private readonly apiUrl: string = 'https://retoolapi.dev/yZjtsj/customers';
+  // private readonly apiUrl: string = 'https://randomuser.me/api';
 
   constructor(private http: HttpClient) {}
 
   //fetch all users
   getUsers(page: number = 1, limit: number = 10): Observable<any> {
-    return this.http.get<any>(`$(this.apiUrl)?_page=${page}&_limit=${limit}`);
+    return this.http.get<any>(`${this.apiUrl}?_page=${page}&_limit=${limit}`);
   }
+  // getUsers(size: number = 10): Observable<any> {
+  //   return this.http.get<any>(`$(this.apiUrl)/?results=${size}`);
+  // }
 
   //fetch one user using idCustomer
   getUser(idCustomer: number = 1): Observable<any> {
-    return this.http.get<any>(`$(this.apiUrl)?id=${idCustomer}`);
+    return this.http.get<any>(`${this.apiUrl}?id=${idCustomer}`);
   }
+  // getUser(uuid: number = 1): Observable<any> {
+  //   return this.http.get<any>(`$(this.apiUrl)/?uuid=${uuid}`);
+  // }
 }
