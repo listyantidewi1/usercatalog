f9f0f6f4d4a849b4faaa2c5482c3082c759322fb
listyantidewi1 <listyantidewi@gmail.com>
2023-11-21 22:45:25 +0700

    comment all the interfaces

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Response } from '../interface/response.interface';
 import { User } from '../interface/user.interface';
+// import { response } from 'express';
 
 @Injectable({
   providedIn: 'root',
@@ -16,6 +17,7 @@ export class UserService {
   //fetch all users
   getUsers(page: number = 1, limit: number = 10): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}?_page=${page}&_limit=${limit}`);
+    // .pipe(map((response) => this.processResponse(response)));
   }
   // getUsers(size: number = 10): Observable<any> {
   //   return this.http.get<any>(`$(this.apiUrl)/?results=${size}`);
@@ -24,23 +26,25 @@ export class UserService {
   //fetch one user using idCustomer
   getUser(idCustomer: number = 1): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}?id=${idCustomer}`);
+    // .pipe(map((response) => this.processResponse(response)));
   }
   // getUser(uuid: number = 1): Observable<any> {
   //   return this.http.get<any>(`$(this.apiUrl)/?uuid=${uuid}`);
   // }
 
-  private processResponse(response: Response): Response {
-    return {
-      results: response.results.map(
-        (user: any) =>
-          <User>{
-            id: user.id,
-            customer_name: user.customer_name,
-            address: user.address,
-            email: user.email,
-            phone_number: user.phone_number,
-          }
-      ),
-    };
-  }
+  // private processResponse(response: Response): Response {
+  //   return {
+  //     // results: response.results.map(
+  //     //   (user: any) =>
+  //     //     <User>{
+  //     //       id: user.id,
+  //     //       customer_name: user.customer_name,
+  //     //       address: user.address,
+  //     //       email: user.email,
+  //     //       phone_number: user.phone_number,
+  //     //     }
+  //     // ),
+  //     // results: response.results,
+  //   };
+  // }
 }
