9ccfb9641b7c84b43b02d785d1dc243f71537a1d
listyantidewi1 <listyantidewi@gmail.com>
2023-11-21 23:28:24 +0700

    display a user detail but fixed on id=1

--- src/app/component/userdetail/userdetail.component.ts
+++ src/app/component/userdetail/userdetail.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { UserDetailService } from './../../service/user-detail.service';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './../../service/user.service';
 
 @Component({
   selector: 'app-userdetail',
   templateUrl: './userdetail.component.html',
   styleUrl: './userdetail.component.css',
 })
-export class UserdetailComponent {}
+export class UserdetailComponent implements OnInit {
+  constructor(private userDetailService: UserDetailService) {}
+  response: Array<any>;
+
+  ngOnInit(): void {
+    this.userDetailService.getUser(1).subscribe((results: any) => {
+      console.log(results);
+      this.response = results;
+    });
+  }
+}
---
+++ src/app/service/user-detail.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDetailService {
+  private readonly apiUrl: string = 'https://retoolapi.dev/yZjtsj/customers';
+
+  constructor(private http: HttpClient) { }
+   //fetch one user using idCustomer
+   getUser(idCustomer: number = 1): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?id=${idCustomer}`);
+    // .pipe(map((response) => this.processResponse(response)));
+  }
+}
